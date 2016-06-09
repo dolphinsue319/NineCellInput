8ee1b6b9c2a8b85ac2aa9a247c4bd891ca402bc0
dolphinSuPixnet <dolphinsu@pixnet.tw>
2016-06-09 11:22:06 +0800

    move deletePreviousInput function

--- GamepadController.py
+++ GamepadController.py
@@ -15,6 +15,21 @@ aWord = ""
 sentence = []
 observing = False
 
+def deletePreviousInput():
+    # 在 currentMode 為 0 時，右搖桿向左要刪除前一個輸入的數字、前一個注音符號、前一個字
+    global twoNumbers
+    global aWord
+    global sentence
+    if len(twoNumbers) == 1:
+        print("delete one number: " + str(twoNumbers))
+        twoNumbers = ""
+    elif len(aWord) > 0:
+        print("delete final symbol in a word: " + aWord[-1])
+        aWord = aWord[:-1]
+    elif len(sentence) > 0:
+        print("delete final word in sentence: " + sentence[-1])
+        sentence = sentence[:-1]
+
 def willChooseDirectionCallback(direction):
     """
     搖桿被移到某一個方向了，且還沒放掉搖桿
@@ -58,21 +73,6 @@ def getClickCallback():
 def willChooseRightDirectionCallback(direction):
     print("willChooseRightDirectionCallback: " + str(direction))
     
-def deletePreviousInput():
-    # 在 currentMode 為 0 時，右搖桿向左要刪除前一個輸入的數字、前一個注音符號、前一個字
-    global twoNumbers
-    global aWord
-    global sentence
-    if len(twoNumbers) == 1:
-        print("delete one number: " + str(twoNumbers))
-        twoNumbers = ""
-    elif len(aWord) > 0:
-        print("delete final symbol in a word: " + aWord[-1])
-        aWord = aWord[:-1]
-    elif len(sentence) > 0:
-        print("delete final word in sentence: " + sentence[-1])
-        sentence = sentence[:-1]
-
 def didChooseRightDirectionCallback(direction):
     print("didChooseRightDirectionCallback: " + str(direction))
     global currentMode
